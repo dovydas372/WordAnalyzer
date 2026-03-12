2b1b33d901f5d896951981de4f72d1d8ead7df92
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2026-03-12 21:19:25 +0200

    Add try catch error's and file path

--- WordAnalyzer/Program.cs
+++ WordAnalyzer/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"C:\Users\PMAC\Desktop\PROGRAMAVIMAS\C# pamokos\tekstas.txt";
+
+            try
+            {
+                string fileText = File.ReadAllText(filePath);
+
+             
+
+                
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Failas nerastas.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Nėra teisės pasiekti šio failo.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Įvyko klaida: {ex.Message}");
+            }
         }
+
+       
     }
 }
